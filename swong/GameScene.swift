44e1db5b830e759bbc005355e9a2b8922a1cdf5c
Cor Pruijs <cor@CorBook-Yosemite.local>
2014-07-31 22:00:52 +0200

    Added ColliderType enum and added categorybitmasks

--- swong/GameScene.swift
+++ swong/GameScene.swift
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     //for delta time
 //    var lastUpdateTimeInterval: NSTimeInterval(NaN)
@@ -21,6 +21,14 @@ class GameScene: SKScene {
     var currentDirection                        = CGVectorMake(5, 5)
     var lastUpdateTimeInterval: CFTimeInterval  = 0
     
+    enum ColliderType: UInt32 {
+        case Ball = 1
+        case Paddle = 2
+        case Devbox = 3
+        case Leveledge = 8
+    }
+    
+    
     
     var fpsCount         = 0
     
@@ -28,39 +36,45 @@ class GameScene: SKScene {
 
         
         //SCENE (SELF)
-        self.backgroundColor            = SKColor(red: 0.31, green: 0.39, blue: 0.4, alpha: 1)
-        self.scaleMode                  = SKSceneScaleMode.Fill
-        self.physicsBody                = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        self.physicsBody.dynamic        = false
-        self.physicsBody.friction       = 0
-        self.physicsWorld.gravity       = CGVectorMake(0, 0)
+        self.backgroundColor                = SKColor(red: 0.31, green: 0.39, blue: 0.4, alpha: 1)
+        self.scaleMode                      = SKSceneScaleMode.Fill
+        self.physicsWorld.gravity           = CGVectorMake(0, 0)
+        self.physicsWorld.contactDelegate   = self
+        self.physicsBody                    = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody.dynamic            = false
+        self.physicsBody.friction           = 0
+        self.physicsBody.categoryBitMask    = ColliderType.Leveledge.toRaw()
+        self.physicsBody.contactTestBitMask = ColliderType.Ball.toRaw()
         
         
         // BALL
-        ball.size                       = CGSizeMake(25, 25)
-        ball.position                   = CGPointMake(self.frame.midX, self.frame.midY)
-        ball.physicsBody                = SKPhysicsBody(circleOfRadius: ball.size.height / 2)
-        ball.physicsBody.dynamic        = true
-        ball.physicsBody.allowsRotation = false
-        ball.physicsBody.linearDamping  = 0
-        ball.physicsBody.velocity       = CGVectorMake(30, 0)
+        ball.size                           = CGSizeMake(25, 25)
+        ball.position                       = CGPointMake(self.frame.midX, self.frame.midY)
+        ball.physicsBody                    = SKPhysicsBody(circleOfRadius: ball.size.height / 2)
+        ball.physicsBody.dynamic            = true
+        ball.physicsBody.allowsRotation     = false
+        ball.physicsBody.linearDamping      = 0
+        ball.physicsBody.velocity           = CGVectorMake(30, 0)
+        ball.physicsBody.categoryBitMask    = ColliderType.Ball.toRaw()
+        ball.physicsBody.contactTestBitMask = ColliderType.Leveledge.toRaw() | ColliderType.Paddle.toRaw() | ColliderType.Devbox.toRaw()
         
         self.addChild(ball)
         
         //DEVBOX
-        devbox.size                     = CGSizeMake(75, 75)
-        devbox.position                 = CGPointMake(self.frame.midX + 0.25 * self.frame.width, self.frame.midY)
-        devbox.physicsBody              = SKPhysicsBody(rectangleOfSize: devbox.size)
-        devbox.physicsBody.dynamic      = false
-        devbox.physicsBody.mass         = 0
+        devbox.size                         = CGSizeMake(75, 75)
+        devbox.position                     = CGPointMake(self.frame.midX + 0.25 * self.frame.width, self.frame.midY)
+        devbox.physicsBody                  = SKPhysicsBody(rectangleOfSize: devbox.size)
+        devbox.physicsBody.dynamic          = false
+        devbox.physicsBody.categoryBitMask  = ColliderType.Devbox.toRaw()
+        devbox.physicsBody.contactTestBitMask = ColliderType.Ball.toRaw()
         self.addChild(devbox)
         
         
         //NAME LABEL
-        gamenameLabel.text              = "Swong"
-        gamenameLabel.fontSize          = 30
-        gamenameLabel.position          = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        gamenameLabel.zPosition         = -10
+        gamenameLabel.text                  = "Swong"
+        gamenameLabel.fontSize              = 30
+        gamenameLabel.position              = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        gamenameLabel.zPosition             = -10
         self.addChild(gamenameLabel)
         
         // DEBUG LABEL
@@ -99,6 +113,12 @@ class GameScene: SKScene {
     }
    
 
+    func didBeginContact(contact: SKPhysicsContact!)  {
+        println("contact!")
+        
+        println(contact.description)
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         self.debugLabel.text = "x: \(Int(self.ball.position.x)) y: \(Int(self.ball.position.y))"
 

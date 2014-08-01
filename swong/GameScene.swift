15a82ed03ecaecd20f0d29615cacfebd096f5f9b
Cor Pruijs <cor@CorBook-Yosemite.local>
2014-08-01 21:57:19 +0200

    added more debug info and used it to fix some bugs

--- swong/GameScene.swift
+++ swong/GameScene.swift
@@ -22,7 +22,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let paddle1scoreLabel                       = SKLabelNode(fontNamed: "Helvetica")
     let paddle2scoreLabel                       = SKLabelNode(fontNamed: "Helvetica")
     let gamenameLabel                           = SKLabelNode(fontNamed: "Helvetica")
-    let debugLabel                              = SKLabelNode(fontNamed: "Helvetica")
+    let debugLabelPosition                      = SKLabelNode(fontNamed: "Helvetica")
+    let debugLabelVelocity                      = SKLabelNode(fontNamed: "Helvetica")
+    let debugLabelOther                         = SKLabelNode(fontNamed: "Helvetica")
     let movespeed                               = 500.0
     let movespeedMultiplier                     = 30
     let verticalMoveSpeedAtStart                = 0
@@ -55,94 +57,119 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
         
         //SCENE (SELF)
-        self.backgroundColor                    = SKColor(red: 0.31, green: 0.39, blue: 0.4, alpha: 1)
-        self.scaleMode                          = SKSceneScaleMode.Fill
-        self.physicsWorld.gravity           	= CGVectorMake(0, 0)
-        self.physicsWorld.contactDelegate       = self
-        self.physicsBody                        = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        self.physicsBody.dynamic                = false
-        self.physicsBody.friction               = 0
-        self.physicsBody.restitution            = 1
-        self.physicsBody.categoryBitMask        = ColliderType.Leveledge.toRaw()
-        self.physicsBody.contactTestBitMask     = ColliderType.Ball.toRaw()
+        self.backgroundColor                            = SKColor(red: 0.31, green: 0.39, blue: 0.4, alpha: 1)
+        self.scaleMode                                  = SKSceneScaleMode.Fill
+        self.physicsWorld.gravity                       = CGVectorMake(0, 0)
+        self.physicsWorld.contactDelegate               = self
+        self.physicsBody                                = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody.dynamic                        = false
+        self.physicsBody.friction                       = 0
+        self.physicsBody.restitution                    = 1
+        self.physicsBody.categoryBitMask                = ColliderType.Leveledge.toRaw()
+        self.physicsBody.contactTestBitMask             = ColliderType.Ball.toRaw()
         
         
         // BALL
-        ball.size                               = CGSizeMake(50, 50)
-        ball.position                           = CGPointMake(self.frame.midX, self.frame.midY)
-        ball.physicsBody                        = SKPhysicsBody(circleOfRadius: ball.size.height / 2)
-        ball.physicsBody.dynamic                = true
-        ball.physicsBody.allowsRotation         = false
-        ball.physicsBody.linearDamping          = 0
-        ball.physicsBody.velocity               = CGVectorMake(CGFloat(movespeed), CGFloat(verticalMoveSpeedAtStart))
-        ball.physicsBody.categoryBitMask        = ColliderType.Ball.toRaw()
-        ball.physicsBody.contactTestBitMask     = ColliderType.Leveledge.toRaw() | ColliderType.Paddle.toRaw() | ColliderType.Devbox.toRaw() | ColliderType.Wall1.toRaw() | ColliderType.Wall2.toRaw()
-        
+        ball.size                                       = CGSizeMake(50, 50)
+        ball.position                                   = CGPointMake(self.frame.midX, self.frame.midY)
+        ball.physicsBody                                = SKPhysicsBody(circleOfRadius: ball.size.height / 2)
+        ball.physicsBody.dynamic                        = true
+        ball.physicsBody.allowsRotation                 = false
+        ball.physicsBody.linearDamping                  = 0
+        ball.physicsBody.velocity                       = CGVectorMake(CGFloat(movespeed), CGFloat(verticalMoveSpeedAtStart))
+        ball.physicsBody.categoryBitMask                = ColliderType.Ball.toRaw()
+        ball.physicsBody.contactTestBitMask             = ColliderType.Leveledge.toRaw() | ColliderType.Paddle.toRaw() | ColliderType.Devbox.toRaw() | ColliderType.Wall1.toRaw() | ColliderType.Wall2.toRaw()
         self.addChild(ball)
         
         
         //PADDLE 1
-        paddle1.size                            = CGSizeMake(50, 150)
-        paddle1.position                        = CGPointMake((self.frame.width - paddle1.size.width), self.frame.midY)
-        paddle1.physicsBody                     = SKPhysicsBody(rectangleOfSize: paddle1.size)
-        paddle1.physicsBody.dynamic             = true
-        paddle1.physicsBody.allowsRotation      = false
-        paddle1.physicsBody.linearDamping       = 0
-        paddle1.physicsBody.restitution         = 1
-        paddle1.physicsBody.mass                = 10000000000
-        paddle1.physicsBody.categoryBitMask     = ColliderType.Paddle.toRaw()
-        paddle1.physicsBody.contactTestBitMask  = ColliderType.Ball.toRaw()
+        paddle1.size                                    = CGSizeMake(50, 150)
+        paddle1.position                                = CGPointMake((self.frame.width - paddle1.size.width), self.frame.midY)
+        paddle1.physicsBody                             = SKPhysicsBody(rectangleOfSize: paddle1.size)
+        paddle1.physicsBody.dynamic                     = true
+        paddle1.physicsBody.allowsRotation              = false
+        paddle1.physicsBody.linearDamping               = 0
+        paddle1.physicsBody.restitution                 = 1
+        paddle1.physicsBody.mass                        = 10000000000
+        paddle1.physicsBody.categoryBitMask             = ColliderType.Paddle.toRaw()
+        paddle1.physicsBody.contactTestBitMask          = ColliderType.Ball.toRaw()
         self.addChild(paddle1)
         
         //PADDLE 2
-        paddle2.size                            = CGSizeMake(50, 150)
-        paddle2.position                        = CGPointMake(paddle2.size.width, self.frame.midY)
-        paddle2.physicsBody                     = SKPhysicsBody(rectangleOfSize: paddle2.size)
-        paddle2.physicsBody.dynamic             = true
-        paddle2.physicsBody.allowsRotation      = false
-        paddle2.physicsBody.linearDamping       = 0
-        paddle2.physicsBody.restitution         = 1
-        paddle2.physicsBody.mass                = 10000000000
-        paddle2.physicsBody.categoryBitMask     = ColliderType.Paddle.toRaw()
-        paddle2.physicsBody.contactTestBitMask  = ColliderType.Ball.toRaw()
+        paddle2.size                                    = CGSizeMake(50, 150)
+        paddle2.position                                = CGPointMake(paddle2.size.width, self.frame.midY)
+        paddle2.physicsBody                             = SKPhysicsBody(rectangleOfSize: paddle2.size)
+        paddle2.physicsBody.dynamic                     = true
+        paddle2.physicsBody.allowsRotation              = false
+        paddle2.physicsBody.linearDamping               = 0
+        paddle2.physicsBody.restitution                 = 1
+        paddle2.physicsBody.mass                        = 10000000000
+        paddle2.physicsBody.categoryBitMask             = ColliderType.Paddle.toRaw()
+        paddle2.physicsBody.contactTestBitMask          = ColliderType.Ball.toRaw()
         self.addChild(paddle2)
 
         //WALL 1 
-        wall1.position                          = CGPointMake(self.frame.width - (0.5 * wall2.size.width), self.frame.midY)
-        wall1.physicsBody                       = SKPhysicsBody(rectangleOfSize: wall1.size)
-        wall1.physicsBody.dynamic               = false
-        wall1.physicsBody.categoryBitMask       = ColliderType.Wall1.toRaw()
+        wall1.position                                  = CGPointMake(self.frame.width - (0.5 * wall2.size.width), self.frame.midY)
+        wall1.physicsBody                               = SKPhysicsBody(rectangleOfSize: wall1.size)
+        wall1.physicsBody.dynamic                       = false
+        wall1.physicsBody.categoryBitMask               = ColliderType.Wall1.toRaw()
         self.addChild(wall1)
         
         //WALL 2
-        wall2.position                          = CGPointMake(0.5 * wall1.size.width, self.frame.midY)
-        wall2.physicsBody                       = SKPhysicsBody(rectangleOfSize: wall2.size)
-        wall2.physicsBody.dynamic               = false
-        wall2.physicsBody.categoryBitMask       = ColliderType.Wall2.toRaw()
+        wall2.position                                  = CGPointMake(0.5 * wall1.size.width, self.frame.midY)
+        wall2.physicsBody                               = SKPhysicsBody(rectangleOfSize: wall2.size)
+        wall2.physicsBody.dynamic                       = false
+        wall2.physicsBody.categoryBitMask               = ColliderType.Wall2.toRaw()
         self.addChild(wall2)
         
         // PADDLE 1 SCORE LABEL
-        paddle1scoreLabel.text                  = "\(self.paddle1score)"
-        paddle1scoreLabel.fontSize              = 60
-        paddle1scoreLabel.position              = CGPointMake(self.frame.midX + 100, self.frame.height - 100)
-        paddle1scoreLabel.zPosition             = -10
+        paddle1scoreLabel.text                          = "\(self.paddle1score)"
+        paddle1scoreLabel.fontSize                      = 60
+        paddle1scoreLabel.position                      = CGPointMake(self.frame.midX + 100, self.frame.height - 100)
+        paddle1scoreLabel.zPosition                     = -10
         self.addChild(paddle1scoreLabel)
         
         // PADDLE 2 SCORE LABEL
-        paddle2scoreLabel.text                  = "\(self.paddle2score)"
-        paddle2scoreLabel.fontSize              = 60
-        paddle2scoreLabel.position              = CGPointMake(self.frame.midX - 100, self.frame.height - 100)
-        paddle2scoreLabel.zPosition             = -10
+        paddle2scoreLabel.text                          = "\(self.paddle2score)"
+        paddle2scoreLabel.fontSize                      = 60
+        paddle2scoreLabel.position                      = CGPointMake(self.frame.midX - 100, self.frame.height - 100)
+        paddle2scoreLabel.zPosition                     = -10
         self.addChild(paddle2scoreLabel)
         
         //NAME LABEL
-        gamenameLabel.text                      = "Swong"
-        gamenameLabel.fontSize                  = 60
-        gamenameLabel.position                  = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        gamenameLabel.zPosition                 = -10
-        
-        
+        gamenameLabel.text                              = "Swong"
+        gamenameLabel.fontSize                          = 60
+        gamenameLabel.position                          = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        gamenameLabel.zPosition                         = -10
         self.addChild(gamenameLabel)
+        
+        //DEBUG LABEL POSITION
+        debugLabelPosition.text                         = "POSITION x: \(Int(ball.position.x)) y: \(Int(ball.position.y))"
+        debugLabelPosition.fontSize                     = 20
+        debugLabelPosition.position                     = CGPoint(x: 0, y: 0)
+        debugLabelPosition.horizontalAlignmentMode      = SKLabelHorizontalAlignmentMode.Left
+        debugLabelPosition.verticalAlignmentMode        = SKLabelVerticalAlignmentMode.Bottom
+        debugLabelPosition.zPosition                    = -10
+        self.addChild(debugLabelPosition)
+        
+        //DEBUG LABEL VELOCITY
+        debugLabelVelocity.text                         = "VELOCITY dx: \(Int(ball.physicsBody.velocity.dx)) dy: \(Int(ball.physicsBody.velocity.dy))"
+        debugLabelVelocity.fontSize                     = 20
+        debugLabelVelocity.position                     = CGPoint(x: 0, y: 20)
+        debugLabelVelocity.horizontalAlignmentMode      = SKLabelHorizontalAlignmentMode.Left
+        debugLabelVelocity.verticalAlignmentMode        = SKLabelVerticalAlignmentMode.Bottom
+        debugLabelVelocity.zPosition                    = -10
+        self.addChild(debugLabelVelocity)
+        
+        //DEBUG LABEL OTHER
+        debugLabelOther.text                            = "RESETTING: \(self.ballIsResetting)"
+        debugLabelOther.fontSize                        = 20
+        debugLabelOther.position                        = CGPoint(x: 0, y: 42)
+        debugLabelOther.horizontalAlignmentMode         = SKLabelHorizontalAlignmentMode.Left
+        debugLabelOther.verticalAlignmentMode           = SKLabelVerticalAlignmentMode.Bottom
+        debugLabelOther.zPosition                       = -10
+        self.addChild(debugLabelOther)
+        
         
         // RESET ANIMATION
         let wait1 = SKAction.moveTo(CGPointMake(self.frame.midX, self.frame.height - self.ball.size.height), duration: self.waitduration / 3)
@@ -184,9 +211,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     func didBeginContact(contact: SKPhysicsContact!)  {
         
-        if contact.bodyA.categoryBitMask == ColliderType.Paddle.toRaw() && contact.bodyB.categoryBitMask == ColliderType.Ball.toRaw() && !ballIsResetting {
-            
-            self.ballIsResetting = true
+        if contact.bodyA.categoryBitMask == ColliderType.Paddle.toRaw() && contact.bodyB.categoryBitMask == ColliderType.Ball.toRaw() {
+            ballIsResetting = false
+
             if ball.position.x > self.frame.midX {
                 ball.physicsBody.velocity.dx = (CGFloat(self.movespeed) + paddleHitCount * movespeedMultiplier ) * -1
             } else if ball.position.x < self.frame.midX {
@@ -196,14 +223,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
         }
         
-        if contact.bodyA.categoryBitMask == ColliderType.Wall1.toRaw() && contact.bodyB.categoryBitMask == ColliderType.Ball.toRaw() {
+        if contact.bodyA.categoryBitMask == ColliderType.Wall1.toRaw() && contact.bodyB.categoryBitMask == ColliderType.Ball.toRaw() && !ballIsResetting {
+            ballIsResetting = true
             paddle2score++
             paddle2scoreLabel.text = "\(self.paddle2score)"
             resetBall()
 
         }
         
-        if contact.bodyA.categoryBitMask == ColliderType.Wall2.toRaw() && contact.bodyB.categoryBitMask == ColliderType.Ball.toRaw() {
+        if contact.bodyA.categoryBitMask == ColliderType.Wall2.toRaw() && contact.bodyB.categoryBitMask == ColliderType.Ball.toRaw() && !ballIsResetting {
+            ballIsResetting = true
             paddle1score++
             paddle1scoreLabel.text = "\(self.paddle1score)"
             resetBall()
@@ -217,13 +246,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
         }
     }
-//    
-//    override func update(currentTime: CFTimeInterval) {
-//        
-////        self.debugLabel.text = "x: \(Int(self.ball.position.x)) y: \(Int(self.ball.position.y))"
-////        self.debugLabel.text = "speed: \(Int(self.ball.physicsBody.velocity.dx))"
-//
-//    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        
+        debugLabelPosition.text = "POSITION x: \(Int(ball.position.x)) y: \(Int(ball.position.y))"
+        debugLabelVelocity.text = "VELOCITY dx: \(Int(ball.physicsBody.velocity.dx)) dy: \(Int(ball.physicsBody.velocity.dy))"
+        debugLabelOther.text    = "RESETTING: \(self.ballIsResetting) PADDLEHITCOUNT: \(self.paddleHitCount)"
+
+    }
     
     func resetBall() {
         self.ball.position = CGPointMake(self.frame.midX, self.frame.midY)
@@ -240,6 +270,5 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             self.ball.physicsBody.velocity.dx = CGFloat(self.movespeed)
         }
         self.ball.physicsBody.velocity.dy = CGFloat(self.verticalMoveSpeedAtStart)
-        ballIsResetting = false
     }
 }
